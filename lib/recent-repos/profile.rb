ecd0dd28e2fc48ce6902ee0c03ea62cfce737efa
Brandon Weaver <a7xncob@gmail.com>
2019-11-26 22:51:31 +0000

    Fixed CSS selectors

--- lib/recent-repos/profile.rb
+++ lib/recent-repos/profile.rb
@@ -2,11 +2,11 @@ class RecentRepos::Profile
   attr_accessor :name, :contributions, :repositories
   
   def initialize(scraper)
-    @name = scraper.documents[:profile].css(".p-name").first.text
+    @name = scraper.documents[:profile].css("span.p-name.vcard-fullname.d-block.overflow-hidden").text
     @contributions = scraper.documents[:profile].css("h2.f4.text-normal.mb-2")[1].text.split(' ')[0]
     
     @repositories = []
-    scraper.documents[:repositories].css("div.d-inline-block h3").each { |repo|
+    scraper.documents[:repositories].css("div.d-inline-block.mb-1 h3 a").each { |repo|
       @repositories << repo.text.split(' ')[0]
     }
   end
