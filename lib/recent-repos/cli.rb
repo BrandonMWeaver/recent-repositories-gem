af9aa9d24d54b72d06c978d165ca6ca44455fd13
Brandon Weaver <a7xncob@gmail.com>
2019-11-29 01:19:36 +0000

    Added a menu system/Simplified bin/recent-repos

--- lib/recent-repos/cli.rb
+++ lib/recent-repos/cli.rb
@@ -1,9 +1,11 @@
 class RecentRepos::CLI
   attr_accessor :profile
   
-  def initialize(profile)
-    @profile = profile
+  def call
+    menu
   end
+  
+  private
   
   def print_name
     puts @profile.name
@@ -15,6 +17,29 @@ class RecentRepos::CLI
   
   def print_repositories
     puts "Recent repositories:"
-    @profile.repositories.each { |repo| puts "\t#{repo}" }
+    i = 0
+    @profile.repositories.each { |repo|
+      print "\t"
+      print '0' if i < 9
+      puts "#{i += 1}. #{repo}"
+    }
+  end
+  
+  def menu
+    loop do
+      puts "Enter a command:"
+      input = gets.strip
+      if input.split(':')[0].downcase == "search"
+        @profile = RecentRepos::Profile.new(RecentRepos::Scraper.new(input.split(':')[1].split(' ')[0]))
+        print_name
+      end
+      if input.downcase == "contributions" && @profile
+        print_contributions
+      end
+      if input.downcase == "repos" && @profile
+        print_repositories
+      end
+      break if input.downcase == "exit" 
+    end
   end
 end
